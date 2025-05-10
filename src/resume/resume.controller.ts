58990000771d6096df9267f72bb0f89fc8abae1c
FindlayMB <calgaryfinz@gmail.com>
2025-05-10 16:25:25 -0600

    Created Resume File upload endpoint

---
+++ src/resume/resume.controller.ts
@@ -0,0 +1,35 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ResumeService } from './resume.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+@Controller('resume')
+export class ResumeController {
+  constructor(private readonly resumeService: ResumeService) {}
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads', // Local folder to save files
+        filename: (req, file, cb) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          cb(null, `${uniqueSuffix}${ext}`);
+        },
+      }),
+    }),
+  )
+  public uploadResume(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    return { message: 'File uploaded successfully!', filePath: file.path };
+  }
+}
